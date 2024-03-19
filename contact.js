28e9e54a27adea0f7ef9b3962ca1d863795bc8bb
akash100499 <akashyakkaluru@gmail.com>
2024-03-19 13:52:53 +0530

    implemneted delete and dynamic Data

--- contact.js
+++ contact.js
@@ -29,10 +29,24 @@ function updateTable() {
     var tbody = document.querySelector('#contactTable tbody');
     tbody.innerHTML = ""; // Clear table body
     var contacts = JSON.parse(localStorage.getItem("contacts")) || [];
-    contacts.forEach(function(contact) {
-        var newRow = "<tr><td>" + contact.name + "</td><td>" + contact.phone + "</td><td>" + contact.email + "</td><td><button class='btn btn-primary'>Edit</button></td><td><button class='btn btn-danger'>Delete</button></td></tr>";
+    contacts.forEach(function(contact, index) { // Added 'index' parameter
+        var newRow = "<tr><td>" + contact.name + "</td><td>" + contact.phone + "</td><td>" + contact.email + "</td><td><button class='btn btn-danger' onclick='confirmDelete(" + index + ")'>Delete</button></td></tr>"; // Passed 'index' to confirmDelete()
         tbody.innerHTML += newRow;
     });
 }
 
-window.onload = updateTable;
+function confirmDelete(index) {
+    if (confirm("Are you sure you want to delete this contact?")) {
+        deleteContact(index);
+    }
+}
+
+function deleteContact(index) {
+    var contacts = JSON.parse(localStorage.getItem("contacts")) || [];
+    contacts.splice(index, 1);
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+    updateTable();
+}
+
+// Call updateTable() on page load
+updateTable();
